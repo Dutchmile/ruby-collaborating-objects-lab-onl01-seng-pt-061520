aa1dc917663ebaa0a63d304ac70fc1ce826b1c9f
Christopher Dutcher <cadutcher@icloud.com>
2020-07-31 10:39:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,26 @@ require 'pry'
 class Song
   attr_accessor :artist, :name
 
+  @@all
+
   def initialize(name)
     @name = name
+    save
   end
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
+  end
+
+  def print_songs(name)
+    artist_songs = []
+    Song.all.collect {|song| artist_songs << song.artist = name}
+    puts artist_songs
+  end
+
+  def save
+    @@all << self
   end
 
   def self.new_by_filename(file)
